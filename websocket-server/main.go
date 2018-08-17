b9aa6981d4afa3f3ab6b5739da39176df2d432f3
Carlos Vasquez <carlos@cobranix.com>
2018-08-16 23:17:02 -0700

    Working websocket server, tracks ws connections and relays messages

--- websocket-server/main.go
+++ websocket-server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("vim-go")
+
+	// Set custom port by running with --port PORT_NUM
+	// Default port is 8000
+	httpPort := flag.String("port", "8000", "WebSocket Listening Address")
+	flag.Parse()
+
+	socketChat := newSocketChat()
+
+	http.HandleFunc("/ws", socketChat.websocketHandler)
+
+	go socketChat.handleMessages()
+
+	log.Println("Starting SocketChat Server")
+	log.Println("Listening on port: ", *httpPort)
+	err := http.ListenAndServe(":"+*httpPort, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
 }
